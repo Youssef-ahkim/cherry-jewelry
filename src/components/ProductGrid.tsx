8c7666b28a2e9940fa67f49bdd8a254b03c41ccb
Youssef-ahim <youssefahkim1234@gmail.com>
2026-06-30 18:20:57 +0100

    center collection grid items and center product details under cards

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -19,30 +19,13 @@ export default function ProductGrid({ products }: { products: Product[] }) {
         </div>
       </FadeIn>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 lg:gap-10 max-w-[1400px] mx-auto">
-        {products.slice(0, 3).map((product, i) => (
+      <div className="flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-10 max-w-[1400px] mx-auto">
+        {products.map((product, i) => (
           <div
             key={product.slug}
-            className={i === 0 ? "sm:col-span-2 lg:col-span-1 lg:row-span-1" : ""}
+            className="w-full sm:w-[calc(50%-16px)] lg:w-[calc(33.333%-27px)] max-w-[420px]"
           >
             <ProductCard product={product} index={i} />
-          </div>
-        ))}
-
-        {products.length > 3 && (
-          <div className="sm:col-span-2 lg:col-span-2">
-            <ProductCard product={products[3]} index={3} />
-          </div>
-        )}
-        {products.length > 4 && (
-          <div>
-            <ProductCard product={products[4]} index={4} />
-          </div>
-        )}
-
-        {products.slice(5).map((product, i) => (
-          <div key={product.slug}>
-            <ProductCard product={product} index={i + 5} />
           </div>
         ))}
       </div>
